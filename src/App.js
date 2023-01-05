de9f40cc162f699658170fb65802e3cbe1293f52
Evgeniy Loznevoy <evgeniy.loznevoy@yandex.ru>
2023-01-05 15:31:21 +0300

    task fix

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ export default function App() {
         <Menu />
         <div className="page">
           <Routes>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/drift" component={DriftPage} />
-            <Route path="/timeattack" component={TimeAttackPage} />
-            <Route path="/forza" component={ForzaPage} />
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
           </Routes>
         </div>
       </div>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,39 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from 'react-router-dom';
 
-export default function Menu() {
-    return (
-        <nav className="menu">
-            <NavLink className="menu__item" activeClassName='menu__item-active' exact to="/">Главная</NavLink>
-            <NavLink className="menu__item" activeClassName='menu__item-active' to="/drift">Дрифт-такси</NavLink>
-            <NavLink className="menu__item" activeClassName='menu__item-active' to="/timeattack">Time Attack</NavLink>
-            <NavLink className="menu__item" activeClassName='menu__item-active' to="/forza">Forza Karting</NavLink>
-        </nav>
-    );
+function Menu() {
+  return (
+    <nav className="menu">
+      <NavLink 
+        className={({ isActive }) => {
+          const linkClasses = ["menu__item"];
+          if (isActive) linkClasses.push("menu__item-active");
+          return linkClasses.join(" ");
+        }} exact="true" to="/">
+          Главная
+      </NavLink>
+      <NavLink className={({ isActive }) => {
+          const linkClasses = ["menu__item"];
+          if (isActive) linkClasses.push("menu__item-active");
+          return linkClasses.join(" ");
+        }} to="/drift">
+          Дрифт-такси
+      </NavLink>
+      <NavLink className={({ isActive }) => {
+          const linkClasses = ["menu__item"];
+          if (isActive) linkClasses.push("menu__item-active");
+          return linkClasses.join(" ");
+        }} to="/timeattack">
+          Time Attack
+      </NavLink>
+      <NavLink className={({ isActive }) => {
+          const linkClasses = ["menu__item"];
+          if (isActive) linkClasses.push("menu__item-active");
+          return linkClasses.join(" ");
+        }} to="/forza">
+          Forza Karting
+      </NavLink>
+    </nav>
+  );
 }
+
+export default Menu;
